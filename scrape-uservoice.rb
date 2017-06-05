a33d6f21cae980ea506c0fd3decd57c302e77a0e
Hossam Hammady <github@hammady.net>
2017-06-05 11:57:49 +0300

    Adding missing abstracts + some code styling

--- scrape-uservoice.rb
+++ scrape-uservoice.rb
@@ -5,7 +5,6 @@ require 'rayyan-formats-core'
 require 'rayyan-formats-plugins'
 require 'dotenv'
 
-
 Dotenv.load
 
 RayyanFormats::Base.plugins = RayyanFormats::Base.available_plugins
@@ -18,26 +17,23 @@ API_SECRET = ENV['USERVOICE_API_SECRET']
 
 client = UserVoice::Client.new(SUBDOMAIN_NAME, API_KEY, API_SECRET)
 
-
-tickets = client.get_collection("/api/v1/tickets.json", :limit => 50) #change the number to the amount of tickets you want per execution
-# puts "Total tickets: #{tickets.size}" 
+tickets = client.get_collection("/api/v1/tickets.json", limit: 10)
+$stderr.puts "Total number of tickets: #{tickets.size}"
 
 tickets.each do |ticket|
-	target = RayyanFormats::Target.new
-	target.sid = ticket['ticket_number'] 
-	target.title = ticket['subject']
-	target.date_array = []
-	d = DateTime.strptime(ticket['created_at'], "%Y/%m/%d")
-	target.date_array << d.year 
-	target.date_array << d.month
-	target.date_array << d.day  
-	target.authors, target.abstracts = [], []
-    messages = ticket['messages']
-    messages.each do |message|    
-		target.authors << message['sender']['name']
-    	target.abstracts << message['body']
-    end
+  target = RayyanFormats::Target.new
+  target.sid = ticket['ticket_number']
+  target.title = ticket['subject']
+  date = DateTime.strptime(ticket['created_at'], "%Y/%m/%d")
+  target.date_array = [date.year, date.month, date.day]
+  target.authors, target.abstracts = [], []
+  messages = ticket['messages']
+  messages.each do |message|
+    author = message['sender']['name']
+    target.authors << author
+    target.abstracts << "#{author} wrote on #{date}: #{message['body']}\n-----\n"
+  end
+  target.authors.uniq!
 
-   puts plugin.export(target)
- end
-
+  puts plugin.export(target, {include_abstracts: true})
+end
